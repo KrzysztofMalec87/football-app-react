12007ee444696c99e1f945c73db93b0bbd308f98
krzysiek <krzysztof@like.agency>
2019-09-01 13:21:26 +0200

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
+
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
