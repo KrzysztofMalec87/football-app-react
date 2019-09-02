3016e2d5569d29c91fbb86a4aee5de163c871079
krzysiek <krzysztof@like.agency>
2019-09-02 20:35:16 +0200

    INIT: wordpress files

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
-import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('football-app'));
-
-serviceWorker.unregister();
