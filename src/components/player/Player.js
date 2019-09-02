371dc6a3fb98fcb2d7958b5c567119e9374abf84
krzysiek <krzysztof@like.agency>
2019-09-02 20:51:36 +0200

    INIT: wordpress files

---
+++ src/components/player/Player.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import PlayerImage from '../../assets/images/player.png';
+import SlideFromRight from '../../common/animations/SlideFromRight';
+
+class Player extends Component {
+  state = {
+    active: false,
+  };
+
+  componentDidMount() {
+    this.setState({ active: true });
+  }
+
+  render() {
+    const { active } = this.state;
+
+    return (
+      <SlideFromRight
+        animationDelay={200}
+        className="c-player"
+        pose={active ? 'visible' : 'hidden'}
+      >
+        <img src={PlayerImage} alt="Pro Futball App playes" />
+      </SlideFromRight>
+    );
+  }
+}
+
+export default Player;
