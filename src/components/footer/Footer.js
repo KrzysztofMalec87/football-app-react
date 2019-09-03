5d243881df6fec3a1fe042450af3882ca30bdaed
krzysiek <krzysztof@like.agency>
2019-09-03 19:23:15 +0200

    Add footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import FadeInTop from '../../common/animations/FadeInTop';
+
+class Footer extends Component {
+  state = {
+    active: false,
+  };
+
+  componentDidMount() {
+    this.setState({ active: true });
+  }
+
+  render() {
+    const { active } = this.state;
+
+    return (
+      <FadeInTop className="c-footer" pose={active ? 'visible' : 'hidden'}>
+        <div className="c-footer__item">
+          App created by <strong>Krzysztof Malec</strong>
+        </div>
+        <div className="c-footer__item"> www.krzysztofmalec.com</div>
+      </FadeInTop>
+    );
+  }
+}
+
+export default Footer;
