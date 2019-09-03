dcd2e3e980031eb4fe7980d8df895ba88d13b594
krzysiek <krzysztof@like.agency>
2019-09-03 20:11:01 +0200

    Add form component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Ball from './components/ball/Ball';
 import Header from './components/header/Header';
 import Player from './components/player/Player';
 import Footer from './components/footer/Footer';
+import Form from './components/form/Form';
 
 const App = () => (
   <>
     <Header />
+    <Form />
     <Player />
     <Ball />
     <Footer />
---
+++ src/components/form/Form.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import FadeInTop from '../../common/animations/FadeInTop';
+
+class Form extends Component {
+  state = {
+    active: false,
+  };
+
+  componentDidMount() {
+    this.setState({ active: true });
+  }
+
+  render() {
+    const { active } = this.state;
+
+    return (
+      <FadeInTop
+        animationDelay={800}
+        className="c-form"
+        pose={active ? 'visible' : 'hidden'}
+      >
+        <form classNanme="c-form">
+          <select className="c-form__input" type="text">
+            <option>Premier League</option>
+            <option>Bundesliga</option>
+          </select>
+        </form>
+      </FadeInTop>
+    );
+  }
+}
+
+export default Form;
