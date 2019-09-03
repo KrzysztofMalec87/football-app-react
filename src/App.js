9cc794f866ebf757820a3210aae072592acd1c1a
krzysiek <krzysztof@like.agency>
2019-09-03 19:24:12 +0200

    Add footer component in to app js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Ball from './components/ball/Ball';
 import Header from './components/header/Header';
 import Player from './components/player/Player';
+import Footer from './components/footer/Footer';
 
 const App = () => (
   <>
     <Header />
     <Player />
     <Ball />
+    <Footer />
   </>
 );
 
