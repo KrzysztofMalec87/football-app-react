9285a93df3ca71448c23ea6f0d1937768034aca3
krzysiek <krzysztof@like.agency>
2019-09-03 19:18:42 +0200

    Add header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import FadeInTop from '../../common/animations/FadeInTop';
+import FadeInBottom from '../../common/animations/FadeInBottom';
+import logo from '../../assets/images/logo.png';
 
-const Header = () => <FadeInTop className="container">smapl</FadeInTop>;
+class Header extends Component {
+  state = {
+    active: false,
+  };
+
+  componentDidMount() {
+    this.setState({ active: true });
+  }
+
+  render() {
+    const { active } = this.state;
+
+    return (
+      <FadeInBottom
+        animationDelay={100}
+        className="c-header"
+        pose={active ? 'visible' : 'hidden'}
+      >
+        <img src={logo} alt="Football App" />
+      </FadeInBottom>
+    );
+  }
+}
 
 export default Header;
